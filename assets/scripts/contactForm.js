d203b87cf0252cac1098d99a06930fd0a2e9efd1
LoKyLj <kyljs3d@gmail.com>
2023-12-07 16:47:11 +0300

    started cart, added modal to contact, updated vid

--- assets/scripts/contactForm.js
+++ assets/scripts/contactForm.js
@@ -1,17 +1,48 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
 
-    async function formSend (e) {
-        e.preventDefault();
+//     async function formSend (e) {
+//         e.preventDefault();
 
-        let error = formValidate(error);
-    }
+//         let error = formValidate(error);
+//     }
 
-    const formValidate = (form) => {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-    }
-});
+//     const formValidate = (form) => {
+//         let error = 0;
+//         let formReq = document.querySelectorAll('._req');
+//     }
+// });
+
+if (document.querySelector('.contact-modal')) {
+    document.addEventListener('DOMContentLoaded', function () {
+        const modal = document.querySelector('.contact-modal');
+        const openBtn = document.querySelector('.form-button');
+        const closeBtn = document.querySelectorAll('.closeButton');
+        const closeBtnBox = document.querySelector('.closeButton-box');
+
+        function openModal(index) {
+            modal.classList.remove('modal--hide');
+            modal.classList.add('modal--display');
+        }
+
+        openBtn.addEventListener('click', openModal);
+        
+        function closeModal() {
+            modal.classList.add('modal--hide');
+        }
+
+        closeBtn.forEach(element => {
+            element.addEventListener('click', closeModal);
+        });
+        closeBtnBox.addEventListener('click', closeModal);
+
+        document.addEventListener('keydown', function (e) {
+            if (e.key === "Escape") {
+                closeModal();
+            }
+        });
+    });
+};
