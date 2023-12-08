6fd6d5d5920e62ff687632167635af09960ba874
LoKyLj <kyljs3d@gmail.com>
2023-12-08 22:52:51 +0300

    added php

--- assets/scripts/contactForm.js
+++ assets/scripts/contactForm.js
@@ -1,48 +1,128 @@
 "use strict";
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
 
-//     async function formSend (e) {
-//         e.preventDefault();
+    async function formSend (e) {
+        e.preventDefault();
 
-//         let error = formValidate(error);
-//     }
+        let error = formValidate(form);
 
-//     const formValidate = (form) => {
-//         let error = 0;
-//         let formReq = document.querySelectorAll('._req');
-//     }
-// });
+        let formData = new FormData(form);
 
-if (document.querySelector('.contact-modal')) {
-    document.addEventListener('DOMContentLoaded', function () {
-        const modal = document.querySelector('.contact-modal');
-        const openBtn = document.querySelector('.form-button');
-        const closeBtn = document.querySelectorAll('.closeButton');
-        const closeBtnBox = document.querySelector('.closeButton-box');
-
-        function openModal(index) {
-            modal.classList.remove('modal--hide');
-            modal.classList.add('modal--display');
-        }
-
-        openBtn.addEventListener('click', openModal);
-        
-        function closeModal() {
-            modal.classList.add('modal--hide');
-        }
-
-        closeBtn.forEach(element => {
-            element.addEventListener('click', closeModal);
-        });
-        closeBtnBox.addEventListener('click', closeModal);
-
-        document.addEventListener('keydown', function (e) {
-            if (e.key === "Escape") {
-                closeModal();
+        if (error===0) {
+            formButton.classList.add('_sending');
+            loaderContainer.classList.add('loader-container--sending');
+            let response = await fetch("send.php", {
+                method: "POST",
+                body: formData
+            });
+            if (response.ok) {
+                formButton.classList.remove('_sending');
+                loaderContainer.classList.remove('loader-container--sending');
+                let result = await response.json();
+                form.reset();
+                openModal();
+            } else {
             }
-        });
-    });
-};
+        } else {
+
+        }
+    }
+
+    const formValidate = (form) => {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq [index];
+            formRemoveError(input);
+
+            if (input.classList.contains('email')) {
+                if (emailTest(input)) { 
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+    const formAddError = (input) => {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    const formRemoveError = (input) => {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    const emailTest = (input) => {
+        return !/^\w+([\ • -]?\w+) *@\w+([\ - - ]?\w+)*(\ • \w{2,8})+$/.test(input.value);
+    }
+});
+
+const modal = document.querySelector('.contact-modal');
+const openBtn = document.querySelector('.form-button');
+const closeBtn = document.querySelectorAll('.closeButton');
+const closeBtnBox = document.querySelector('.closeButton-box');
+const formButton = document.querySelector('.form-button');
+const loaderContainer = document.querySelector('.loader-container');
+
+function openModal(index) {
+    modal.classList.remove('modal--hide');
+    modal.classList.add('modal--display');
+}
+
+function closeModal() {
+    modal.classList.add('modal--hide');
+}
+
+closeBtn.forEach(element => {
+    element.addEventListener('click', closeModal);
+});
+closeBtnBox.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
+
+// async function submitForm(event) {
+//     event.preventDefault(); // отключаем перезагрузку/перенаправление страницы
+//     try {
+//         // Формируем запрос
+//       const response = await fetch(event.target.action, {
+//           method: 'POST',
+//           body: new FormData(event.target)
+//       });
+//       // проверяем, что ответ есть
+//       if (!response.ok) throw (`Ошибка при обращении к серверу: ${response.status}`);
+//       // проверяем, что ответ действительно JSON
+//       const contentType = response.headers.get('content-type');
+//       if (!contentType || !contentType.includes('application/json')) {
+//         throw ('Ошибка обработки. Ответ не JSON');
+//       }
+//       // обрабатываем запрос
+//       const json = await response.json();
+//       if (json.result === "success") {
+//           // в случае успеха
+//           alert(json.info);
+//       } else { 
+//           // в случае ошибки
+//           console.log(json);
+//           throw (json.info);
+//       }
+//     } catch (error) { // обработка ошибки
+//       alert(error);
+//     }
+//   }
--- assets/scripts/products.js
+++
@@ -1,8 +0,0 @@
-const productsDataJSON = fetch('../../data/grass.json');
-const productDataString = JSON.stringify(productsDataJSON);
-
-
-for (let i = 0; i < productsDataJSON.length; i++) {
-    const obj = productsDataJSON[i];
-    console.log(obj.name, obj.id);
-}
